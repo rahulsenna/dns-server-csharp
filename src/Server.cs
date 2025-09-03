e7c9fc41335da518b7add04009c8550df764d976
Rahul Senna <rahulvsenna@gmail.com>
2025-09-03 14:49:10 +0530

    Write answer section #xm2

--- src/Server.cs
+++ src/Server.cs
@@ -26,16 +26,25 @@ while (true)
 
     Console.WriteLine($"Received {ReceivedData.Length} bytes from {SourceEndPoint}: {ReceivedString}");
 
+    byte[] AnswerSection = [
+            0xC0, 0x0C,             // Name: pointer to domain at offset 12
+            0x00, 0x01,             // TYPE: A record (IPv4 address)
+            0x00, 0x01,             // CLASS: Internet
+            0x00, 0x00, 0x01, 0x2C, // TTL: 300 seconds
+            0x00, 0x04,             // RDLENGTH: 4 bytes
+            127, 0, 0, 1            // RDATA: 127.0.0.1 (localhost)
+    ];
 
     byte[] Response = [
             // Header (12 bytes)
             ReceivedData[0], ReceivedData[1], // Transaction ID
-            0x81, 0x80, // Response flags (NOT 0x01, 0x00!)
-            0x00, 0x01, // Question Count: 1
-            0x00, 0x01, // Answer Count: 1 (NOT 0!)
-            0x00, 0x00, // Authority Count: 0
-            0x00, 0x00, // Additional Count: 0
-            ..ReceivedData[12..] // Domain
+            0x81, 0x80,           // Response flags (NOT 0x01, 0x00!)
+            0x00, 0x01,           // Question Count: 1
+            0x00, 0x01,           // Answer Count: 1 (NOT 0!)
+            0x00, 0x00,           // Authority Count: 0
+            0x00, 0x00,           // Additional Count: 0
+            ..ReceivedData[12..], // Question Section
+            ..AnswerSection,      // Answer Section
             ];
     
 
